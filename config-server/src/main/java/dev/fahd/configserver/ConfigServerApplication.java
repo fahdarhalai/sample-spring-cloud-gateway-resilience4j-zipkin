a3d6304dfade3da6f5519e33b81d7999a0c53226
Fahd Arhalai <farhalai@novelis.io>
2021-05-29 20:21:34 +0100

    configuration server

---
+++ config-server/src/main/java/dev/fahd/configserver/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package dev.fahd.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
